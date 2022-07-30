98104e17b3089e56006beedd4c3bccc147d1eac7
freddieswift <freddie9117@live.co.uk>
2022-07-30 15:50:07 +0100

    Alert Styling

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -4,9 +4,10 @@ import { useAppContext } from '../context/AppContext'
 
 const Alert = () => {
     const { alertMessage, alertType } = useAppContext()
+    console.log(alertType)
     return (
         <div
-            className={classes.errorModal}
+            className={`${classes.alert} ${classes['alert-' + alertType]}`}
         >
             {alertMessage}
         </div>
